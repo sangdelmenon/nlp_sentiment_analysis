e7ee35a1a66d6035ca070bf1ee237b627f40babf
sangdelmenon <sangeeth.menon@outlook.com>
2026-02-06 22:39:37 -0500

    Fix SSL issue with NLTK downloads

--- text_processor.py
+++ text_processor.py
@@ -3,6 +3,14 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
 
 # Download necessary NLTK data
 try:
